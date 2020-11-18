1b1415be4aec483c5220b1b9b4ba395e70eb361f
acidevil94 <coluzziandreajh@gmail.com>
2020-11-18 14:14:18 +0000

    added missing annotation from test

--- src/test/java/com/coluzzi/kankan/controller/IssueControllerTest.java
+++ src/test/java/com/coluzzi/kankan/controller/IssueControllerTest.java
@@ -1,6 +1,7 @@
 package com.coluzzi.kankan.controller;
 
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,7 @@ public class IssueControllerTest {
     private MockMvc mvc;
 
 
+    @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                                                     .andExpect(MockMvcResultMatchers.status().isOk())
